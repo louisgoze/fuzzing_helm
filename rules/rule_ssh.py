08d2b771a488b80a41c8fabee06f70528178de6e
aiitzo <160031080+aiitzo@users.noreply.github.com>
2024-02-15 16:12:39 +0100

    Update rule_ssh.py

--- rules/rule_ssh.py
+++ rules/rule_ssh.py
@@ -12,3 +12,8 @@ def fuzz_memory_limit(input_dict) -> dict:
     output_dict["spec.template.spec.containers[0].resources.limits.memory"] = "0m"
     return output_dict
     # "spec/template/spec/containers/[0]/resources/limits/memory"
+
+def fuzz_cpu_limit(input_dict) -> dict:
+    output_dict = copy.deepcopy(input_dict)
+    output_dict["spec.template.spec.containers[0].resources.limits.cpu."] = "0,5m"
+    return output_dict
